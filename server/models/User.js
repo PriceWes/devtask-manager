920cadf3b9adf3fa0d8922a1faed29fe0efe3e71
PriceWes <obonyowesliy@gmail.com>
2025-07-26 16:34:19 +0300

    server changes

--- server/models/User.js
+++ server/models/User.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
     username: {type: String, required: true, unique: true},
-    email: { type: String, require: true, unique: true},
-    password: { type: String, require: true},
+    email: { type: String, required: true, unique: true},
+    password: { type: String, required: true},
     role: { type: String, enum: ["developer", "admin"], default: "developer"}
 });
 
